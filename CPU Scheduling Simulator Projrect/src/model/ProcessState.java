28e99469d7db71c6640126c1015622cc78b136dd
Shubhamsangwa <shubhamsangwa38@gmail.com>
2026-06-13 00:24:57 +0530

    Delete CPU Scheduling Simulator Projrect directory

--- CPU Scheduling Simulator Projrect/src/model/ProcessState.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public enum ProcessState {
-    NEW,
-    READY,
-    RUNNING,
-    WAITING,
-    COMPLETED
-}
--- CPU Scheduling Simulator Projrect/src/scheduler/Scheduler.java
+++
@@ -1,8 +0,0 @@
-package scheduler;
-
-import model.Process;
-import java.util.List;
-
-public interface Scheduler {
-    Process getNextProcess(List<Process> processes, int currentTime);
-}
